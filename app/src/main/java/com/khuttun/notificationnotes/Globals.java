f47c6c2fd44634f8ab8cf575d84c668cc75d3bbd
Nicola Spanti (RyDroid) <dev@nicola-spanti.info>
2015-12-31 19:40:45 +0100

    Very minor Java changes to class Globals

--- app/src/main/java/com/khuttun/notificationnotes/Globals.java
+++ app/src/main/java/com/khuttun/notificationnotes/Globals.java
@@ -1,6 +1,12 @@
 package com.khuttun.notificationnotes;
 
-class Globals
+final class Globals
 {
     public static final String TAG = "Notification Notes";
+    
+    /**
+     * Do not let this class be instantiated.
+     */
+    private Globals()
+    {}
 }
